0ad2e14c0cdc978388147d1578fcbacef735fe6e
Dmitry Fink <dmitry@finik.net>
2021-02-15 16:18:03 -0800

    Hello, world!

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Bugsee",
+    products: [
+        .library(
+            name: "Bugsee",
+            targets: ["Bugsee"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Bugsee",
+            url: "https://download.bugsee.com/sdk/ios/Bugsee-VERSION.zip",
+            checksum: "CHECKSUM")
+    ]
+)
