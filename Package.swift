74fa3f2249e21ddb64fa44aa8f39782cfbd29310
Jenkins Build <build@finikmini.local>
2021-10-19 06:50:09 -0700

    Updating SDK to 1.28.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Bugsee",
-            url: "https://download.bugsee.com/sdk/ios/spm/Bugsee-1.28.0.zip",
-            checksum: "f94307abc41d6e7c95a9005255d8f62d6c866e2411f1d61211700f60cc8d0da0")
+            url: "https://download.bugsee.com/sdk/ios/spm/Bugsee-1.28.1.zip",
+            checksum: "e5ac14f6f062c6f566b9602a2f63b0fc145e2bc625e08c60d84e948717e30b26")
     ]
 )
