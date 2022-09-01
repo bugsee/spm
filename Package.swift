5beb7f8737887256f26136606acda0cb4404779a
Leroy Jenkins <build@MiniM1.local>
2022-09-01 01:58:13 -0700

    Updating SDK to 3.0.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Bugsee",
-            url: "https://download.bugsee.com/sdk/ios/spm/Bugsee-2.1.8.zip",
-            checksum: "c27d22e856503ef36a7dd87141742dd47874a30a157ac5d6e55ef76d0f2b5293")
+            url: "https://download.bugsee.com/sdk/ios/spm/Bugsee-3.0.0.zip",
+            checksum: "9e6edcc5f4f982b5dece93c1ead377c790e5706cd30720c05e6f223b25934a96")
     ]
 )
